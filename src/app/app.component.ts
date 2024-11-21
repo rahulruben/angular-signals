ec7c51dea1bdb2a08d86218515a7763688cee4b6
Rahulruben Mule <mulerahulruben@gmail.com>
2024-11-21 17:15:34 +0530

    Added basic functionality of Signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-signals';
+  quantity = signal<number>(1);
+  qtyAvailable = signal<number[]>([1, 2, 3, 4]);
+
+  onQuantityChanged(qty: number) {
+    this.quantity.set(qty);
+  }
 }
